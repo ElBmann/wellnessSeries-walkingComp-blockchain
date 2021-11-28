637796b81ac65ca62f801af9e0ab68c33e9a8fbc
B <uibmjh8e@anonaddy.me>
2021-11-28 11:15:39 -0500

    Created local block chain environment.
    Added Deploy file

--- contracts/walkCompetition.sol
+++ contracts/walkCompetition.sol
@@ -3,7 +3,17 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract walkCompetition{
+    
+    uint totalMiles;
     constructor(){
         console.log("YOOO");
+    }
+    function miles() public{
+        totalMiles += 1;
+        console.log("Has walked %s ",msg.sender);
+    }
+    function getTotalmiles()public view returns(uint256) {
+         console.log("Current Miles",totalMiles," Walked by ", msg.sender);
+         return totalMiles;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,26 @@
 const main = async ()=>{
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const walkContractFactory = await hre.ethers.getContractFactory('walkCompetition');
     const walkContract = await walkContractFactory.deploy();
     await walkContract.deployed();
-};
 
+    console.log("Contract deployed to:", walkContract.address);
+    console.log("contact deployed by:", owner.address);
+
+let mileCount;
+mileCount = await walkContract.getTotalmiles();
+
+let mileTxn = await walkContract.miles();
+await mileTxn.wait();
+
+mileCount = await walkContract.getTotalmiles();
+
+mileTxn = await walkContract.connect(randomPerson).miles();
+await mileTxn.wait();
+
+mileCount = await walkContract.getTotalmiles();
+
+};
 const runMain = async () =>{
     try{
         await main();
