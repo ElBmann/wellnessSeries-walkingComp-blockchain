ac3b4ad5a29842fd196e5a6f3e8f3947f278da47
B <uibmjh8e@anonaddy.me>
2021-12-02 20:25:06 -0500

    Add .env to contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,8 +21,8 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/9JZb1hY5wd2etpvIzCix_enebrCnHqlf',
-      accounts: ['d7709435436760d495c04f5aac017b7fcdee040fa7b7777e17e8c8c361119fa7'],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
