5b341197da87c562304ffd8446e96d3d4e5cd7c6
B <uibmjh8e@anonaddy.me>
2021-12-04 18:05:04 -0500

    Update to Strava
    Clicking button now takes user to Strava approvals

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
 import { ethers } from "ethers";
 import './App.css';
 import abi from './utils/walkCompetition.json';
 import { default as strava, Strava } from 'strava-v3';
 import fetchMiles from './scripts/getStravaAPI';
+import { Link } from 'react-router-dom'
 require('dotenv').config();
 //Add Front End to Git. Please
 const App = () => {
   const [allMiles, setAllMiles] = useState([]);
   const [currentAccount, setCurrentAccount] = useState("");
+  const [stravaLink, setStravaLink] = useState("");
   const contractAddress = "0x8BEC6b8A62be9E4705755F62136aD710Af10CCe9";
   const contractABI = abi.abi;
 
@@ -23,9 +25,9 @@ const App = () => {
         const WalkCompitionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
         const walks = await WalkCompitionContract.getTotalmiles();
-        console.log("walk.walkedMiles "+ walks);
+        console.log("walk.walkedMiles " + walks);
         const milesCleaned = walks.map(walk => {
-          console.log("walk.walkedMiles "+ walk.walkedMiles);
+          console.log("walk.walkedMiles " + walk.walkedMiles);
           return {
             address: walk.walker,
             timestamp: new Date(walk.timestamp * 1000),
@@ -67,34 +69,37 @@ const App = () => {
       console.log(error);
     }
   }
- 
+
 
   const apiGet = async () => {
     const strava = require('strava-v3');
     strava.config({
-      "access_token"  : process.env.ACCESS_TOKEN,
-      "client_id"     : '74956',
-      "client_secret" : process.env.CLIENT_SECRET,
-      "redirect_uri"  : "http://localhost:3000/",
+      "access_token": process.env.ACCESS_TOKEN,
+      "client_id": '74956',
+      "client_secret": process.env.CLIENT_SECRET,
+      "redirect_uri": "http://localhost:3000/",
     });
     // const payload = await strava.athlete.get(
     //   {'access_token':'d0a4383743e43217fdc9f8ef1dfc225681b8a017'},function(err,payload,limits) {
     //     console.log(payload);
-      
+
     // });
-    const authorizationCode = strava.oauth.getRequestAccessURL({scope:"activity:read"})
-   // const stravaApi = new strava.client('d0a4383743e43217fdc9f8ef1dfc225681b8a017');
-   // const payload = await stravaApi.athlete.get(
-     // {'access_token':'d0a4383743e43217fdc9f8ef1dfc225681b8a017'},function(err,payload,limits) {
-       // console.log(payload);
-      
+    const authorizationCode = strava.oauth.getRequestAccessURL({ scope: "activity:read" })
+    // const stravaApi = new strava.client('d0a4383743e43217fdc9f8ef1dfc225681b8a017');
+    // const payload = await stravaApi.athlete.get(
+    // {'access_token':'d0a4383743e43217fdc9f8ef1dfc225681b8a017'},function(err,payload,limits) {
+    // console.log(payload);
+
     //});
-    console.log(authorizationCode);
-   // const payload = await strava.oauth.getToken("code");
- 
+    setStravaLink(authorizationCode);
+   
     
-  }
+    
+    // const payload = await strava.oauth.getToken("code");
   
+
+  }
+
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -149,27 +154,31 @@ const App = () => {
   }
   useEffect(() => {
     checkIfWalletIsConnected();
-    console.log(window.location.href);
-    var str = window.location.href.search(/code=/);
-    var strScope = window.location.href.search(/&scope/);
-    
-    console.log(window.location.href.substr(str,strScope));
+    apiGet();
+    // console.log(window.location.href);
+    // var str = window.location.href.search(/code=/);
+    // var strScope = window.location.href.search(/&scope/);
+
+    //console.log(window.location.href.substr(str, strScope));
   }, [])
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-          🚶‍♂️ Walking Competetion!
+          🚶‍♂️ Walking competitions!
         </div>
-        
+
         <div className="bio">
-          I am Brian Connect your Ethereum wallet and wave at me, Add your strava username, and pick your start and end date for the walking competetion.
+          I am Brian Connect your Ethereum wallet and wave at me, Add your strava username, and pick your start and end date for the walking competition.
         </div>
 
         <form onSubmit={handleSubmit}>
           <input onChange={(e) => setName(e.target.value)} value={name}></input>
-          <button className="waveButton" onClick={apiGet}>
+          <button className="waveButton" onClick={()=>{
+   
+            window.open(stravaLink, "_blank");
+            }}>
             Set Miles Walked 🙌
           </button>
 
@@ -189,7 +198,7 @@ const App = () => {
           </button>
         )}
 
-        
+
       </div>
     </div>
   );
